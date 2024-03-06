afa948d3a1833bfe63594ab20d18978ea77d6991
Snezhana <taratonsnezana@gmail.com>
2024-03-06 15:12:05 +1000

    2 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,71 +5,118 @@ import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.key.Key.Companion.I
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.dvfu_mobile.presentation.TodoCard
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Row(modifier = Modifier){
-                        Column{
-                            Text(text="Todo title")
-                            Text(text="Todo subtitle")
+                Scaffold(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults
+                                .smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+                            title = {
+                                Text(
+                                    "TODO APP",
+                                    style = MaterialTheme.typography.titleLarge.copy(color = Color.White)
+                                )
+                            })
+                    },
+                    bottomBar = {
+                        NavigationBar {
+                            NavigationBarItem(
+                                selected = true,
+                                onClick = { /*TODO*/ },
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Default.List,
+                                        contentDescription = ""
+                                    )
+                                });
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = { /*TODO*/ },
+                                icon = { Icon(
+                                    imageVector = Icons.Default.Check,
+                                    contentDescription = ""
+                                )})
                         }
-                    }
-                }
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {
+                        }) {
+                            Icon(
+                                Icons.Filled.Add,
+                                "Add todo"
+                            )
+                        }
+                    },
+                    content = { padding ->
+                        Column(
+                            modifier = Modifier
+                                .padding(padding)
+                                .padding(top = 22.dp, start = 7.dp, end = 7.dp),
+                            verticalArrangement = Arrangement.spacedBy(21.dp)
+                        ) {
+                            TodoCard("Вынести мусор", "Ибо воняет")
+                            TodoCard(
+                                "Сходить на пару",
+                                "Сходить на пару по мобильной разработке"
+                            )
+                            TodoCard(
+                                "Сходить на пару",
+                                "Сходить на пару по мобильной разработке"
+                            )
+                            TodoCard(
+                                "Сходить на пару",
+                                "Сходить на пару по мобильной разработке"
+                            )
+                            TodoCard(
+                                "Сходить на пару",
+                                "Сходить на пару по мобильной разработке"
+                            )
+                        }
+                    })
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Android!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Card {
-        Row {
-            Column {
-                Text(text = "Title")
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(text = "Subtitle of todo list")
-            }
-            Icon(
-                    imageVector = Icons.Default.AccountCircle,
-                    contentDescription = "asd",
-                    modifier = Modifier.size(48.dp)
-            )
         }
     }
 }
--- app/src/main/java/com/example/myapplication/presentation/ToDoItem.kt
+++ app/src/main/java/com/example/myapplication/presentation/ToDoItem.kt
@@ -1,37 +1,81 @@
-package com.example.dvfumobile
+package com.example.dvfu_mobile.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
 @Composable
-fun TodoItem() {
-    Card {
-        Row {
-            Column {
-                Text(text = "Title")
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(text = "Subtitle of todo list")
+fun TodoCard(title: String, subtitle: String) {
+    Card(
+        colors = CardDefaults.cardColors(Color.White),
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(vertical = 22.dp, horizontal = 19.dp)) {
+                Text(title, style = MaterialTheme.typography.titleLarge)
+                Text(
+                    subtitle,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.requiredWidth(124.dp),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
-            Icon(
-                    imageVector = Icons.Default.AccountCircle,
-                    contentDescription = "asd",
-                    modifier = Modifier.size(48.dp)
-            )
+            Row(modifier = Modifier.align(Alignment.CenterVertically)) {
+                Icon(
+                    imageVector = Icons.Default.Create,
+                    contentDescription = "Inbox Icon",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 20.dp),
+                )
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Inbox Icon",
+                    modifier = Modifier
+                        .size(40.dp).padding(end = 20.dp),
+                )
+                Icon(
+                    imageVector = Icons.Default.Check,
+                    contentDescription = "Inbox Icon",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 20.dp),
+                )
+            }
+
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview()
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        TodoCard("Android", "Android eto horosho")
+    }
+}
+
+
+
